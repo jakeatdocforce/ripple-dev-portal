fe8cfec796c4a3bd7ddb8017f99bf1bbdee3ad05
Jake Bonham <jake@docforce.io>
2019-06-28 11:31:26 -0700

    Changed to single config
    Added language inputs for token replacement
    Assets load based on target location
    Running build.sh will build all targets with language includes

--- build.sh
+++ build.sh
@@ -8,55 +8,27 @@
 ###
 start=`date +%s`
 
-#
-echo "Building target - English first"
+targets=`dactyl_build -lq | awk '{print $1}'`
 
-dactyl_build
+while read -r line; do
+    echo ""
+    echo "======================================="
+    echo "Building Target: $line"
 
-echo "Building target - JP"
+    if [ "$line" == "en" ]; then
+        echo "Building english first"
+        dactyl_build -q -t "$line" --vars "tool/strings_${line}.yaml"
+    else
+        echo "Building other language"
+        dactyl_build -q -t "$line" --vars "tool/strings_${line}.yaml" --no_static --out_dir out/"$line"
+    fi
 
-#  change to separate config file for each lang
-dactyl_build -c dactyl-config-jp.yml --no_static --out_dir out/jp
+done <<< "$targets"
 
-
-# see how long script takes to build. 
+echo ""
+echo "======================================="
+echo "======================================="
+echo ""
 end=`date +%s`
 runtime=$((end-start))
-
 echo "BUILD TOOK > " $runtime
-
-# start local server for testing. 
-# 
-http-server out
-
-
-
-
-## Steps to create new language
-# 1 copy dactyl-config.yml to dactyl-confgi{newlang}.yml
-# 2 change name and values to new lanugage. 
-# 3 copy and change any needed template files with translations needed.  <- future idea to extract all text from template. 
-# 4 copy and translate any .md files needed in content folder.  
-# 5 make sure each change is reflected in new config file.  Any md: or template: lines
-# 6 add new build call above. 
-
-
-
-#### other notes
-## reason to move from single config
-# - 3k lines already.  Each lang could add ~300 to file
-# - more flexible to edit each lang
-
-# Potential issues
-#  - if English is changed how will we match up with language version of config
-#  - if english md or template file is changed how will we trigger alert to watch and notifiy to update translations. 
-
-### Future ideas
-# refactor/extract all text from template files.  move to sepcfic language file. 
-# separate translations to folders? Vs keeping them all next to each other.  Maybe not an issue. 
-# print report of pages in english
-# print report of pages in (lang) that do match to english
-# print report of modified md or template files for english
-# save running list of past changed files? 
-# remove need for targets: -jp in config files.  As each file is unique to that language this seems redundant?
- 
